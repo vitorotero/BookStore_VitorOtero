423ecc76e418377026507527f27f2d6f75f778ce
VitorOtero <vitorotero@MacBook-Pro-de-Mac-2.local>
2019-12-14 11:35:49 +0000

    organizando o projeto e configurando a view controller inicial

---
+++ BookStore_VitorOtero/SupportFiles/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  BookStore_VitorOtero
+//
+//  Created by VitorOtero on 14/12/2019.
+//  Copyright © 2019 VitorOtero. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.tintColor = UIColor.gray
+        
+        self.window = window
+        
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().backgroundColor = .clear
+        UINavigationBar.appearance().isTranslucent = true
+        
+        let navigationController = UINavigationController(rootViewController: BooksViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        return true
+        
+    }
+}
+
